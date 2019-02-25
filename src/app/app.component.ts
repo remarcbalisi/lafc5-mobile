84f8c8363fee8dcd47d49bfafde9215519f2cfd5
Regine Laguting <reginelaguting091998@gmail.com>
2019-02-26 02:51:02 +0800

    import 5 pages (agent page)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component, ViewChild } from '@angular/core';
 import { Platform, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AgentTabscontrollerPage } from '../pages/agent-tabscontroller/agent-tabscontroller';
 
 
 
 // import { ApplyLeavePage } from '../pages/apply-leave/apply-leave';
-import { LoginPage } from '../pages/login/login';
+// import { LoginPage } from '../pages/login/login';
 
 
 
@@ -15,7 +16,7 @@ import { LoginPage } from '../pages/login/login';
 })
 export class MyApp {
   @ViewChild(Nav) navCtrl: Nav;
-    rootPage:any = LoginPage;
+    rootPage:any = AgentTabscontrollerPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
